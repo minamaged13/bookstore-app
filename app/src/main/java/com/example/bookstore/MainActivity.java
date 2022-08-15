e01ff5ccaded3e5c448cf58f64f3c420b04947c1
mina x <mina20191702074@cis.asu.edu.eg>
2022-08-15 13:52:52 +0200

    intents and toast done

--- app/src/main/java/com/example/bookstore/MainActivity.java
+++ app/src/main/java/com/example/bookstore/MainActivity.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,8 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //new account button
         final Button NewAccount =(Button) findViewById(R.id.NewaccountButton);
-
         NewAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -23,10 +25,29 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         }
+        );
+        //submit button
+        final Button Submit =(Button) findViewById(R.id.SubmitButton);
+
+        Submit.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                EditText login_username=(EditText)findViewById(R.id.login_Username);
+                EditText login_password=(EditText)findViewById(R.id.login_password);
+                if(!(login_username.getText().toString().equals(""))&&!(login_password.getText().toString().equals(""))){
+                     //put data validation and database here
 
 
+                    Intent i =new Intent(MainActivity.this,Library.class);
+                    startActivity(i);
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"username or password are empty",Toast.LENGTH_LONG).show();
+                }
 
-
+            }
+        }
         );
     }
 }
--- app/src/main/java/com/example/bookstore/NewAccount.java
+++ app/src/main/java/com/example/bookstore/NewAccount.java
@@ -1,5 +1,6 @@
 package com.example.bookstore;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,6 +10,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class NewAccount extends AppCompatActivity {
 
@@ -19,14 +23,33 @@ public class NewAccount extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Button newacc_submit=(Button)findViewById(R.id.NewaccSubmitBtn);
+        newacc_submit.setOnClickListener(new View.OnClickListener() {
+                     @Override
+                     public void onClick(View view) {
+                     EditText newacc_name=(EditText)findViewById(R.id.newacc_name);
+                     EditText newacc_age=(EditText)findViewById(R.id.newacc_age);
+                     EditText newacc_username=(EditText)findViewById(R.id.newacc_username);
+                     EditText newacc_password=(EditText)findViewById(R.id.newacc_password);
+                     EditText newacc_confirmPassword=(EditText)findViewById(R.id.newacc_confirmPassword);
+
+                     if(!(newacc_username.getText().toString().equals(""))&&!(newacc_password.getText().toString().equals(""))&&!(newacc_confirmPassword.getText().toString().equals(""))&&!(newacc_age.getText().toString().equals(""))&&!(newacc_name.getText().toString().equals(""))){
+                        if(newacc_password.getText().toString().equals(newacc_confirmPassword.getText().toString())){
+                            //password = confirm password
+                        }
+                        else{
+                            Toast.makeText(getApplicationContext(),"password and confirm password are not the same",Toast.LENGTH_LONG).show();
+                        }
+                         //put data validation and database here
+
+                     }
+                     else{
+                         Toast.makeText(getApplicationContext(),"please fill the empty fields",Toast.LENGTH_LONG).show();
+                     }
+
+                     }
+        }
+        );
     }
 
 }
